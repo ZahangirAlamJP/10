12edd763c073701292374794ac417b3dcad9dc34
ZahangirAlamJP <thezahangir@gmail.com>
2022-02-23 03:55:59 +0600

    Make Contact List BY TextEditingController

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,14 @@ void main(List<String> args) {
   ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   final myStyle = TextStyle(fontSize: 20, color: Colors.black);
+
   final List<String> names = [
     'ali',
     'Savid',
@@ -19,28 +25,44 @@ class MyApp extends StatelessWidget {
     "alim"
   ];
 
+  TextEditingController inputC = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('My Contacts'),
       ),
-      body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-        ...List.generate(
-          names.length,
-          (index) => Card(
-            elevation: 5,
-            color: Colors.amber,
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Text(
-                names[index],
-                style: myStyle,
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListView(
+            //  crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                controller: inputC,
+                onEditingComplete: () {
+                  print(inputC.text);
+                  setState(() {
+                    names.add(inputC.text);
+                  });
+                },
               ),
-            ),
-          ),
-        ),
-      ]),
+              ...List.generate(
+                names.length,
+                (index) => Card(
+                  elevation: 5,
+                  color: Colors.amber,
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text(
+                      names[index],
+                      style: myStyle,
+                    ),
+                  ),
+                ),
+              ),
+            ]),
+      ),
     );
   }
 }
