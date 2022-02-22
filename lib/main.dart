4e2a33e861c15d54cdf4d333ae041324e1d1cd99
ZahangirAlamJP <thezahangir@gmail.com>
2022-02-23 03:40:42 +0600

    List.generate & names.length

--- lib/main.dart
+++ lib/main.dart
@@ -25,24 +25,22 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(
         title: Text('My Contacts'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: names
-            .map(
-              (e) => Card(
-                elevation: 5,
-                color: Colors.amber,
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Text(
-                    e,
-                    style: myStyle,
-                  ),
-                ),
+      body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+        ...List.generate(
+          names.length,
+          (index) => Card(
+            elevation: 5,
+            color: Colors.amber,
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text(
+                names[index],
+                style: myStyle,
               ),
-            )
-            .toList(),
-      ),
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
